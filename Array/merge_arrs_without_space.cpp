e2467f23cc1414d90b874521d0c53157d155bd2e
Naman <namanddev7@gmail.com>
2021-08-17 14:15:52 +0530

    fix

--- Array/merge_arrs_without_space.cpp
+++ Array/merge_arrs_without_space.cpp
@@ -13,7 +13,7 @@ void merge_arr(int a1[],int a2[],int n,int m) {
 	int i=0,j=0;
 	int *ele1,*ele2;
 
-	while(i<n || j<m) {
+	while(i<n+m && j<m) {
 		if(i<n) ele1 = &a1[i];
 		else ele1 = &a2[i-n];
 
@@ -23,15 +23,20 @@ void merge_arr(int a1[],int a2[],int n,int m) {
 		else if(*ele1 > *ele2) {
 			swap(ele1,ele2);
 			i++;
-			if(i>=n) j++;
 
+			int k = j+1, l = j;
+			while(k<m && a2[l] > a2[k]) {
+				swap(&a2[l++],&a2[k++]);
+			}
+
+			if(i>n-1) j++;
 		}
 	}
 }
 
 int main() {
 	int arr1[] = {1,3,5,7};
-	int arr2[] = {0,2,6,8,9};
+	int arr2[] = {0,3,5,8,9};
 
 	merge_arr(arr1,arr2,4,5);
 
--- Array/min_jumps.cpp
+++ Array/min_jumps.cpp
@@ -3,28 +3,25 @@
 using namespace std;
 
 int min_jumps(int arr[],int n) {
-	int jumps = 1,prev_pass=arr[0],forward_pass = 0;
+	int jumps = 1,prev_pass=arr[0],forward_pass = arr[0];
 	
 	if(prev_pass==0) return -1;
 
 	for(int i=1; i<n; i++) {
 		
-		if(i==n) break;
+		if(i==n-1) break;
 
 		prev_pass--;
-		if(forward_pass>prev_pass) forward_pass--;
+		forward_pass--;
 		forward_pass=max(arr[i],forward_pass);
 		
 		if(prev_pass==0) {
-			if(forward_pass>0){ 
-				prev_pass = forward_pass;
-				forward_pass=max(arr[i],forward_pass);
-				jumps++;
-			}
-			else {
-				jumps = -1;
-				break;
-			}
+			prev_pass = forward_pass;
+			jumps++;
+
+			if(prev_pass==0) return -1;
+
+			if(prev_pass+i >= n-1) break;
 		}
 
 	}
@@ -32,8 +29,8 @@ int min_jumps(int arr[],int n) {
 }
 
 int main() {
-	int arr[] = {3,0,0,0,0,0};
-	int j = min_jumps(arr,6);
+	int arr[] = {1,3,5,8,9,2,6,7,6,8,9};
+	int j = min_jumps(arr,11);
 
 	cout<<j<<endl; 
 }
