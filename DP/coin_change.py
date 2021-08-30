662cd8f0dfc12b4ffa175ecce40a01ed779a1bd3
Naman <namanddev7@gmail.com>
2021-08-31 02:52:43 +0530

    init DP

---
+++ DP/coin_change.py
@@ -0,0 +1,20 @@
+
+
+def calCoinChange(n,a):
+
+	dp = [0]*(n+1)
+	dp[0] = 1
+
+	for i in a:
+		for j in range(len(dp)):
+			if i <= j:
+				dp[j]+=dp[j-i]
+	return dp[n]
+				
+n = int(intput())
+m = int(input())
+a = list(map(int,input().split()))
+
+total_ways = calCoinChange(n,a)
+
+print(total_ways,end='\n')
