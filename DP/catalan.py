89277e87cdfd716b53fa338c736ff7342bc0ecb9
Naman <namanddev7@gmail.com>
2021-09-12 18:07:04 +0530

    add question list

--- DP/catalan.py
+++ DP/catalan.py
@@ -1,7 +1,14 @@
+#https://www.geeksforgeeks.org/program-nth-catalan-number/
 
+import nCr
 
+# O(n)
+# c(n) = (2nCn)/(n+1)
+def catalan2(n):
+	c = nCr.bioCoeff(2*n,n)
+	return c/(n+1)
 
-
+# O(n^2) 
 # c(1) = 1 && c(n+1) = sum(c(i)*c(n-1), i=0 to n))
 def catalan(n):
 	if n==0 or n==1:
@@ -16,5 +23,6 @@ def catalan(n):
 	return dp[n]
 
 n = int(input())
-ans = catalan(n)
-print(ans,end="\n")
+ans1 = catalan(n)
+ans2 = catalan2(n)
+print(ans1,ans2,end="\n")
--- DP/nCr.py
+++ DP/nCr.py
@@ -1,5 +1,22 @@
 # https://practice.geeksforgeeks.org/problems/ncr1019/1
 
+# O(n)
+def bioCoeff(n,k):
+
+	# C(n,k) = C(n, n-k)
+	if(k>n-k):
+		k = n - k
+
+	res = 1
+	
+	# (n-k+1)!/k!
+	for i in range(k):
+		res = res * (n-i)
+		res = res / (i+1)
+	return res
+
+
+# O(n^2)
 def solve(n,r):
 	if r > n:
 		return 0
@@ -7,19 +24,18 @@ def solve(n,r):
 	dp = [[0 for i in range(n+1)] for _ in range(r+1)]
 
 	for i in range(n+1):
-		for j in range(min(i,r)+1)
-		if i == 0 or j == 0:
-			dp[i][j] = 1
-		else:
-			dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+		for j in range(min(i,r)+1):
+			if i == 0 or j == 0:
+				dp[i][j] = 1
+			else:
+				dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
 
 	bio = int(dp[n][r]%(pow(10,9)+7))
 
 	return bio
 
-n = int(input())
-r = int(input())
-
-nCr = solve(n,r)
-
-print(nCr,end='\n')
+if __name__ == '__main__':
+	n = int(input())
+	r = int(input())
+	nCr = solve(n,r)
+	print(nCr,end='\n')
